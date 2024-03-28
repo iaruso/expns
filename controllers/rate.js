b7ae0b8f618a55f3e8174ae1e89b9b46f84298be
Yaraslau <iaruso.me@gmail.com>
2024-03-28 15:20:29 +0000

    Changed cron schedule

--- controllers/rate.js
+++ controllers/rate.js
@@ -44,7 +44,7 @@ async function fetchAndStoreExchangeRates() {
   }
 }
 
-cron.schedule('20 15 * * *', () => fetchAndStoreExchangeRates());
+cron.schedule('0 0 0 * * *', () => fetchAndStoreExchangeRates());
 
 async function getRates(req, res) {
   try {
